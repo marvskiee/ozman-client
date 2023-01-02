4441db8a1da2ee136ad96294b27b971021435866
Marvi Lean Negrete <acemonkey2@gmail.com>
2023-01-02 08:20:14 +0800

    vercel pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import pluginRewriteAll from 'vite-plugin-rewrite-all';
+import pluginRewriteAll from "vite-plugin-rewrite-all";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -13,7 +13,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(),pluginRewriteAll()],
+  plugins: [react(), pluginRewriteAll()],
   build: {
     rollupOptions: {
       output: {
